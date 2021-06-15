ee3ad7079c255c1b5a19343436246cdabedc781d
Ashwin Padmakumar <ashwinpadmakumar@gmail.com>
2021-06-15 18:13:05 +0530

    Build fix

--- source/urlshortener/run.sh
+++ source/urlshortener/run.sh
@@ -4,18 +4,18 @@ export CONTAINER_ID="${CONTAINER_ID:-local}"
 
 ./gradlew --status
 
-LIVE_RELOAD="${LIVE_RELOAD:-false}"
-if [ "${LIVE_RELOAD}" =  "true" ]; then
-  export GRADLE_OPTS=--daemon
-  export GRADLE_USER_HOME=/tmp/gradle_user_home
-  echo "Live Reload Enabled"
-  ./gradlew --stop
-  ./gradlew build --continuous --quiet --build-cache --parallel -x check -x test -x bootJar &
-else
-  if [ -f "/tmp/gradle_user_home/gradle.properties" ]; then
-      cp /tmp/gradle_user_home/gradle.properties ~/.gradle/
-  fi
-fi
+#LIVE_RELOAD="${LIVE_RELOAD:-false}"
+#if [ "${LIVE_RELOAD}" =  "true" ]; then
+#  export GRADLE_OPTS=--daemon
+#  export GRADLE_USER_HOME=/tmp/gradle_user_home
+#  echo "Live Reload Enabled"
+#  ./gradlew --stop
+#  ./gradlew build --continuous --quiet --build-cache --parallel -x check -x test -x bootJar &
+#else
+#  if [ -f "/tmp/gradle_user_home/gradle.properties" ]; then
+#      cp /tmp/gradle_user_home/gradle.properties ~/.gradle/
+#  fi
+#fi
 
 JVM_ARGS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006"
 JVM_ARGS="${JVM_ARGS} -DLOG_LEVEL=DEBUG"
@@ -28,6 +28,8 @@ JVM_ARGS="${JVM_ARGS} -Xscmaxaot100m"
 JVM_ARGS="${JVM_ARGS} -PCONTAINER_ID=${CONTAINER_ID}"
 
 SPRING_ARG="--spring.profiles.active=local"
+
+echo JVM_ARGS "${JVM_ARGS}"
 echo SPRING_ARG "${SPRING_ARG}"
 
 ./gradlew bootRun --args="${SPRING_ARG}" -PjvmArgs="${JVM_ARGS}"
